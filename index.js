9f48eedd3d9fa9edda146b1ae87d625ddb826a3e
chaolingting <10724352+chaolingting@users.noreply.github.com>
2025-10-30 16:59:58 +0800

    update js html

--- index.js
+++ index.js
@@ -1,49 +1,98 @@
-console.log('hello world')
-const library = document.querySelector(".library");
-const dialog = document.querySelector("dialog");
-const addBtn = document.querySelector("dialog + .add");
-const closeBtn = document.querySelector("dialog .close")
-const form = document.querySelector("form");
-
-
-addBtn.addEventListener('click', () => {
-    dialog.showModal();
-})
-
-closeBtn.addEventListener('click', () =>{
-    dialog.close();
-})
-
-form.addEventListener('submit', (e) => {
-   e.preventDefault();
-    console.log("Add clicked, form submitted!");
-    dialog.close();
-
-})
-
-const myLibrary = [];
-
-  // the constructor
+// the constructor
 function Book(title, author, page, read){
     this.title = title;
     this.author = author;
     this.page = page;
     this.read = read;
+    this.id = crypto.randomUUID();
 
     this.info = function(){
         const readStatus = this.read ? "read" : "not yet";
         return `${title} ${author} ${page} pages, ${readStatus} `
-    }
-}
-
-function addBookToLibrary(){
-    const newBook = Book();
-    newBook.push()
-}
+    };
 
 
-function display(cards){
-    for(let i =0; i< cards.length ; i++ ){
-        
-    }
+};
+
+const myLibrary = [];
+
+
+function addBookToLibrary(title, author, page, read){
+    const newBook = new Book(title, author, page, read);
+    myLibrary.push(newBook);
+
+    createBookCard(newBook);
 }
+
+
+function createBookCard(book){
+        const bookCard = document.createElement('div');
+        bookCard.classList.add('book-card')
+
+        const titleEL = document.createElement('p');
+        titleEL.textContent = `Name: ${book.title}`
+
+        const authorEL = document.createElement('p');
+        authorEL.textContent = `Author: ${book.author}`
+
+        const pageEL = document.createElement('p');
+        pageEL.textContent = `${book.page} pages`
+
+        const readStatusEL = document.createElement('p');
+        readStatusEL.textContent = `Status: ${book.read ? "read" : "not yet"}`;
+
+        bookCard.appendChild(titleEL);
+        bookCard.appendChild(authorEL);
+        bookCard.appendChild(pageEL);
+        bookCard.appendChild(readStatusEL);
+
+        library.appendChild(bookCard);
+}
+
+function display(){
+    myLibrary.forEach((book,index) => {
+        createBookCard(book);
+    })
+}
+
+
+const library = document.querySelector(".library");
+
+//dialog
+const dialog = document.querySelector("dialog");
+const addBtn = document.querySelector("dialog + .add");
+const closeBtn = document.querySelector("dialog .close");
+
+//form
+const form = document.querySelector("form");
+
+    addBtn.addEventListener('click', () => {
+        dialog.showModal();
+    });
+    closeBtn.addEventListener('click', () =>{
+        dialog.close();
+    });
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+        console.log("Add clicked, form submitted!");
+        dialog.close();
+
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const page = document.querySelector('#page').value;
+    const read = document.querySelector('#read').checked;
+
+
+    addBookToLibrary(title, author, page, read);
+
+    form.reset(); 
+    dialog.close();
+
+});
+
+
+
+
+
+
